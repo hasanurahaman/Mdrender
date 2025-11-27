fe3d6351139e313f88b8d3c1d133eba18b5a9101
Mohammad Hasanur Rahaman <hasanuroppof19@gmail.com>
2025-11-27 17:36:30 +0530

    Update base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: 'Mdrender'
+    base: '/Mdrender/'
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
